614905e61d55043f2a2d77f8d2a5792ab9ea74c0
Brandon Dong <bdong5@ualberta.ca>
2024-08-26 12:56:04 -0600

    Integrate Firebase for retrieving and handling book data.

--- app/src/main/java/com/bdong5/mylibrary/BookActivity.java
+++ app/src/main/java/com/bdong5/mylibrary/BookActivity.java
@@ -15,6 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.util.Util;
+import com.bdong5.mylibrary.Book;
+
 
 import java.util.ArrayList;
 
@@ -41,132 +44,179 @@ public class BookActivity extends AppCompatActivity {
 
 
         Intent intent = getIntent();
-        if (null != intent){
-            int bookId = intent.getIntExtra(BOOK_ID_KEY, -1);
-            if (bookId != -1){
-                Book incomingBook = Utils.getInstance(this).getBookById(bookId);
-                if (null != incomingBook){
-                    setData(incomingBook);
-                    handleAlreadyRead(incomingBook);
-                    handleWantToReadBooks(incomingBook);
-                    handleCurrentlyReadingBooks(incomingBook);
-                    handleFavoriteBooks(incomingBook);
-                }
-            }
-        }
-    }
+        if (intent!=null){
 
-    private void handleFavoriteBooks(Book incomingBook) {
-        ArrayList<Book> favoriteBooks = Utils.getInstance(this).getFavoriteBooks();
-        boolean existInFavoriteBooks = false;
-        for (Book b: favoriteBooks){
-            if (b.getId() == incomingBook.getId()){
-                existInFavoriteBooks = true;
-            }
-        }
-        if (existInFavoriteBooks){
-            btnAddtoFavourite.setEnabled(false);
-        } else{}
-    }
+            // Extract the book ID passed from the previous activity
+            String bookId = intent.getStringExtra(BOOK_ID_KEY);
 
-    private void handleCurrentlyReadingBooks(Book incomingBook) {
-        ArrayList<Book> currentlyReadingBooks = Utils.getInstance(this).getCurrentlyReadingBooks();
-        boolean existInCurrentlyReadingBooks = false;
-        for (Book b: currentlyReadingBooks) {
-            if (b.getId() == incomingBook.getId()) {
-                existInCurrentlyReadingBooks = true;
-            } else{
-                btnAddtoFavourite.setOnClickListener(new View.OnClickListener() {
+            if (bookId!=null && !bookId.isEmpty()){     // Check if the book ID is not null and not empty
+
+                Utils.getInstance(this).getBookById(bookId, new Utils.FirebaseCallback<Book>() { // Obtain book details from Firebase using bookId
                     @Override
-                    public void onClick(View v) {
-                            if (Utils.getInstance(BookActivity.this).addToFavorite(incomingBook)){
-                                Toast.makeText(BookActivity.this, "Book Added", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(BookActivity.this, FavoriteBookActivity.class);
-                                startActivity(intent);
-                            } else {
-                                Toast.makeText(BookActivity.this, "Something went wrong. Please try again", Toast.LENGTH_SHORT).show();
-                            }
+                    public void onCallback(Book result) { // If book is successfully retrieved, handle:
+                        if (result != null){
+
+                            setData(result); // Populate UI with book data
+                            handleAlreadyRead(result); // Check if book is already in list
+                            handleWantToReadBooks(result); // ^^
+                            handleCurrentlyReadingBooks(result); // ^^
+                            handleFavoriteBooks(result); // ^^
                         }
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        Toast.makeText(BookActivity.this, "Failed to load book", Toast.LENGTH_SHORT).show();
+                    }
                 });
             }
         }
+        }
 
-        if (existInCurrentlyReadingBooks){
-            btnAddToCurrentlyReading.setEnabled(false);
-        } else{
-            btnAddToCurrentlyReading.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (Utils.getInstance(BookActivity.this).addToCurrentlyReading(incomingBook)){
-                        Toast.makeText(BookActivity.this, "Book Added", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(BookActivity.this, CurrentlyReadingBookActivity.class);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(BookActivity.this, "Something went wrong. Please try again", Toast.LENGTH_SHORT).show();
+    private void handleFavoriteBooks(Book incomingBook) {
+       Utils.getInstance(this).getFavoriteBooks(new Utils.FirebaseCallback<ArrayList<Book>>() { // obtain favorite books from Firebase
+            @Override
+            public void onCallback(ArrayList<Book> favoriteBooks) { // handle favorite books from Firebase
+                boolean existInFavoriteBooks = false;
+                for (Book b: favoriteBooks){ // Check if book exists in favorite books list
+                    if (b.getId().equals(incomingBook.getId())){
+                        existInFavoriteBooks = true; // book exists
+                        break;
                     }
                 }
-            });
-        }
+                if (existInFavoriteBooks){
+                    btnAddtoFavourite.setEnabled(false); // disable button if book is in list
+                } else{
+                    btnAddtoFavourite.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Utils.getInstance(BookActivity.this).addToFavorite(incomingBook);
+                            Toast.makeText(BookActivity.this, "Book Added", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(BookActivity.this, FavoriteBookActivity.class));
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Toast.makeText(BookActivity.this, "Sorry, an error occured. Please try again.", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+    }
+
+    private void handleCurrentlyReadingBooks(Book incomingBook) {
+        Utils.getInstance(this).getCurrentlyReadingBooks(new Utils.FirebaseCallback<ArrayList<Book>>() { // get currently reading books from firebase
+            @Override
+            public void onCallback(ArrayList<Book> currentlyReadingBooks) { // handle currently reading books
+                boolean existInCurrentlyReadingBooks = false;
+                for (Book b: currentlyReadingBooks){
+                    if (b.getId().equals(incomingBook.getId())){
+                        existInCurrentlyReadingBooks = true;
+                        break;
+                    }
+                }
+                if (existInCurrentlyReadingBooks){
+                    btnAddToCurrentlyReading.setEnabled(false); // disable button if book is in list
+                }
+                else{
+                    btnAddToCurrentlyReading.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Utils.getInstance(BookActivity.this).addToCurrentlyReading(incomingBook);
+                            Toast.makeText(BookActivity.this, "Book Added", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(BookActivity.this, CurrentlyReadingBookActivity.class)); // Open currently reading screen
+
+                        }
+                    });
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Toast.makeText(BookActivity.this, "Sorry, an error occured. Please try again.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 
     private void handleWantToReadBooks(Book incomingBook) {
-        ArrayList<Book> wantToReadBooks = Utils.getInstance(this).getWantToReadBooks();
-        boolean existInWantToReadBooks = false;
-        for (Book b: wantToReadBooks) {
-            if (b.getId() == incomingBook.getId()) {
-                existInWantToReadBooks = true;
-            }
-        }
-
-        if (existInWantToReadBooks){
-            btnAddToWantToReadList.setEnabled(false);
-        } else{
-            btnAddToWantToReadList.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (Utils.getInstance(BookActivity.this).addToWantToRead(incomingBook)){
-                        Toast.makeText(BookActivity.this, "Book Added", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(BookActivity.this, WantToReadBookActivity.class);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(BookActivity.this, "Something went wrong. Please try again", Toast.LENGTH_SHORT).show();
+        Utils.getInstance(this).getWantToReadBooks(new Utils.FirebaseCallback<ArrayList<Book>>() { // obtain want to read books from Firebase
+            @Override
+            public void onCallback(ArrayList<Book> wantToReadBooks) { // handle want to read books
+                boolean existInWantToReadBooks = false;
+                for (Book b : wantToReadBooks) {
+                    if (b.getId() == incomingBook.getId()) {
+                        existInWantToReadBooks = true;
+                        break;
                     }
                 }
-            });
-        }
+                if (existInWantToReadBooks) {
+                    btnAddToWantToReadList.setEnabled(false); // disable button if book is in list
+                } else {
+                    btnAddToWantToReadList.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Utils.getInstance(BookActivity.this).addToWantToRead(incomingBook);
+                            Toast.makeText(BookActivity.this, "Book successfully added.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(BookActivity.this, WantToReadBookActivity.class));
+                        }
+                    });
+                }
 
+
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Toast.makeText(BookActivity.this, "Sorry, an error occured. Please try again.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
 
     /** Enable and Disable Button
      * Add the book to Already Read Book ArrayList
      * @param incomingBook
      */
     private void handleAlreadyRead(Book incomingBook) {
-        ArrayList<Book> alreadyReadBooks = Utils.getInstance(this).getAlreadyReadBooks();
-        boolean existInAlreadyReadBooks = false;
-        for (Book b: alreadyReadBooks) {
-            if (b.getId() == incomingBook.getId()) {
-                existInAlreadyReadBooks = true;
-            }
-        }
-
-        if (existInAlreadyReadBooks){
-            btnAddToAlreadyReadList.setEnabled(false);
-        } else{
-            btnAddToAlreadyReadList.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (Utils.getInstance(BookActivity.this).addToAlreadyRead(incomingBook)){
-                        Toast.makeText(BookActivity.this, "Book Added", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(BookActivity.this, AlreadyReadBookActivity.class);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(BookActivity.this, "Something went wrong. Please try again", Toast.LENGTH_SHORT).show();
+        Utils.getInstance(this).getAlreadyReadBooks(new Utils.FirebaseCallback<ArrayList<Book>>() { // obtain already read books from Firebase
+            @Override
+            public void onCallback(ArrayList<Book> alreadyReadBooks) { // handle already read books
+                boolean existInAlreadyReadBooks = false;
+                for (Book b: alreadyReadBooks) {
+                    if (b.getId() == incomingBook.getId()) {
+                        existInAlreadyReadBooks = true;
+                        break;
                     }
                 }
-            });
-        }
+
+                if (existInAlreadyReadBooks){
+                    btnAddToAlreadyReadList.setEnabled(false); // disable button if book is in list
+                } else{
+                    btnAddToAlreadyReadList.setOnClickListener(new View.OnClickListener() { // enable click action
+                        @Override
+                        public void onClick(View v) {
+                            Utils.getInstance(BookActivity.this).addToAlreadyRead(incomingBook);
+                            Toast.makeText(BookActivity.this, "Book Added", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(BookActivity.this, AlreadyReadBookActivity.class));
+                            }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Toast.makeText(BookActivity.this, "Something went wrong. Please try again", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 
@@ -177,6 +227,8 @@ public class BookActivity extends AppCompatActivity {
         txtDescription.setText(book.getLongDesc());
         Glide.with(this).asBitmap().load(book.getImageURL()).into(imgBook);
     }
+
+
 
     private void initViews() {
         txtAuthorName = findViewById(R.id.txtAuthorName);
